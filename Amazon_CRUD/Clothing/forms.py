ca8e618cc91e719d764fa530465ad557c87f6916
SarthakShinde17 <sarthakshinde05@gmail.com>
2025-04-20 21:57:42 +0530

    Add Image Operation

--- Amazon_CRUD/Clothing/forms.py
+++ Amazon_CRUD/Clothing/forms.py
@@ -5,10 +5,5 @@ from django import forms
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', 'description', 'stock']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
-        }
+        fields = ['name', 'price', 'description', 'stock', 'image']   
+        
--- Amazon_CRUD/Clothing/models.py
+++ Amazon_CRUD/Clothing/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
