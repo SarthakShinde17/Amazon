5177ff3e8af63b45745106e7a1cc447afac330b2
SarthakShinde17 <sarthakshinde05@gmail.com>
2025-04-20 20:15:31 +0530

    send product details to frontend

--- Amazon_CRUD/Clothing/views.py
+++ Amazon_CRUD/Clothing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'Clothing/home.html')
+    prod = Product.objects.all()
+    return render(request, 'Clothing/home.html', {'prod': prod})
